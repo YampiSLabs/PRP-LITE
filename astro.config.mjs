b4edd1ecef770dea19f1287a315e98eb44a8bb39
Yampi <51342050+cdryampi@users.noreply.github.com>
2026-05-13 23:20:17 +0200

    fix: configure GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://yampislabs.github.io/PRP-LITE',
+  site: 'https://yampislabs.github.io',
+  base: '/PRP-LITE',
   integrations: [tailwind()],
 });
